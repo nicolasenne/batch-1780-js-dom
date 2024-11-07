bdaca03980b41481b6d63fded12dc9d782ec1004
Nicolas Enne <40183143+nicolasenne@users.noreply.github.com>
2024-11-07 11:17:16 -0300

    Add files via upload

---
+++ javascript/application.js
@@ -0,0 +1,38 @@
+// console.log("Hello from JavaScript!");
+// console.log("Bom dia!");
+
+// const word = document.querySelector(".text-center");
+// const hello = document.querySelector("#hello");
+// const hello2 = document.getElementById("hello");
+
+// console.log(word);
+// console.log(hello);
+// console.log(hello2);
+
+// const list2 = document.getElementById("players");
+
+
+// const list = document.querySelector("#players");
+// list.insertAdjacentHTML("beforeend", "<li class='laura'>Laura</li>");
+// list.insertAdjacentHTML("beforeend", "<li class='paty'>Paty</li>");
+
+// const paty = list.querySelector('.paty');
+
+// console.log(paty.innerText);
+
+// const fifa = document.querySelectorAll('#fifa-wins li');
+// console.log(fifa);
+
+
+// const list = document.querySelector("#fifa-wins");
+// list.insertAdjacentHTML("beforeend", "<li>France (2 wins)</li>");
+
+// // list.style.display = "none";
+
+// list.classList.add("red");
+
+// console.log(list);
+
+const email = document.querySelector("#email");
+console.log(email.value);
+console.log(email.name);
---
+++ ruby.rb
@@ -0,0 +1,30 @@
+# name = "Roberto"
+# hello = "Hello #{name}"
+
+# puts hello
+
+# number = "Laura".to_i
+
+# puts number
+
+
+# array = ["Paty", "Dudu", "Laura"]
+# p array[1]
+
+
+# array = {
+#   js: "Paty",
+#   oop: "Dudu",
+#   db: "Laura"
+# }
+
+# puts array[:db]
+
+
+# age = 18
+
+# if age >= 18
+#   puts "Posso dirigir"
+# else
+#   puts "Não posso dirigir ):"
+# end
